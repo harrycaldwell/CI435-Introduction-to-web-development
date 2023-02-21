ab419a0fffbefe097208a70fb181a65ad1cd252b
Harry Caldwell <harrycaldwell2011@hotmail.co.uk>
2023-02-21 12:43:24 +0000

    session3
    this is lab 3 of sumester 2, i dont know what happened but it broke lol

---
+++ summester_2/session03/index.js
@@ -0,0 +1,25 @@
+var x = 3
+var y = 5
+
+console.log(x<=y);
+console.log(x>4<y);
+console.log(x>4||y>4 );
+console.log(!x>4||y>4);
+console.log(x!=y&y>0);
+console.log(x*y<15);
+console.log(x*y<=15);
+
+if (x>y){
+    console.log("X is greater than Y");
+    console.log(x/y);
+};
+
+while(y<100){
+    x+y
+    console.log(y)
+}
+
+for(let i = 0;i<y; i++){
+    console.log("hello"+i);
+}
+
